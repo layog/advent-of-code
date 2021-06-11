d7065da5571d0e7cf81059c1e1117556dfc16784
Ankit Goyal <layog@google.com>
2021-06-12 00:49:43 +0530

    Add solution for 15-1

---
+++ 2020/15_1.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+use std::io::Read;
+
+fn main() -> std::io::Result<()> {
+    let mut buffer = String::new();
+    std::io::stdin().read_to_string(&mut buffer)?;
+
+    let nums: Vec<u32> = buffer
+        .trim()
+        .split(',')
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let mut m: HashMap<u32, u32> = HashMap::new();
+    let mut count: u32 = 1;
+    let mut prev: u32 = 0;
+
+    for num in nums {
+        let x = m.entry(num).or_insert(count);
+        prev = count - *x;
+        *x = count;
+        count += 1;
+    }
+
+    while count < 2020 {
+        let x = m.entry(prev).or_insert(count);
+        prev = count - *x;
+        *x = count;
+        count += 1;
+    }
+
+    println!("{}", prev);
+
+    Ok(())
+}
